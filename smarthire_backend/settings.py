a118bf2f8829a88741d1a31de4054638bcf8737e
SlammyTee <ogunniranabdulsalam@gmail.com>
2026-05-18 15:10:45 +0100

    changes made

--- smarthire_backend/settings.py
+++ smarthire_backend/settings.py
@@ -164,11 +164,11 @@ WSGI_APPLICATION = 'smarthire_backend.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("PGDATABASE"),
-        "USER": os.getenv("PGUSER"),
-        "PASSWORD": os.getenv("PGPASSWORD"),
-        "HOST": os.getenv("PGHOST"),
-        "PORT": os.getenv("PGPORT"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
