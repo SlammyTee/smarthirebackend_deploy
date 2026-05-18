2867972a0270778230fd67d8bd09e2d6ffab506c
SlammyTee <ogunniranabdulsalam@gmail.com>
2026-05-18 15:28:29 +0100

    changes made

--- smarthire_backend/settings.py
+++ smarthire_backend/settings.py
@@ -172,12 +172,12 @@ DATABASES = {
     }
 }
 
-if os.getenv("RENDER"):
-    DATABASES["default"] = dj_database_url.config(
-        conn_max_age=600,
-        ssl_require=True
-    )
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
