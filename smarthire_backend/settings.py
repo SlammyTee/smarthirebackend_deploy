8b69cc11ebf76056b49d7b702aaf98d82fa71c82
SlammyTee <ogunniranabdulsalam@gmail.com>
2026-05-19 09:00:52 +0100

    change csrf

--- smarthire_backend/settings.py
+++ smarthire_backend/settings.py
@@ -34,9 +34,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ALLOWED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 CORS_ALLOW_CREDENTIALS = True
 
